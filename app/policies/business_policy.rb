6f8e1a11d862ba84444de745b17871b743e9fcca
Alexia Ashton <ashtonalexia@gmail.com>
2021-03-30 17:12:02 +0200

    business controller

---
+++ app/policies/business_policy.rb
@@ -0,0 +1,24 @@
+class BusinessPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    user.present?
+  end
+
+  def update?
+    user.present?
+  end
+
+  def destroy
+    user.present?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'components', to: 'pages#components'
-  
-  resources :businesses
-
+  resources :users do
+    resources :businesses, only: [ :index, :show, :new, :create, :update, :destroy ]
+  end
+  resources :businesses, only: [ :index, :show ]
 end
