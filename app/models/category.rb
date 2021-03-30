859194302a96c4df8b8755eb1c38169559f7b32e
Alexia Ashton <ashtonalexia@gmail.com>
2021-03-30 18:06:38 +0200

    fixed validation syntax error

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, :presence :true
-  validates :image, :presence :true
+  validates :name, presence: true
+  validates :image, presence: true
 
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,10 +1,10 @@
 class Schedule < ApplicationRecord
   belongs_to :business
-  
-  validates :opens, :presence true:
-  validates :closes, :presence true:
-  
 
-  
+  validates :opens, presence: true
+  validates :closes, presence: true
+
+
+
 
 end
