c3b2ba7d995c367b08cb5db4fb53d7f97241e168
Bruno Silva <info.bsilva@gmail.com>
2021-03-30 17:53:23 +0200

    Validation - UNTESTED

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  validates :name, :presence :true
+  validates :image, :presence :true
+
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,5 @@
 class Schedule < ApplicationRecord
+  validates :opens, :presence true:
+  validates :closes, :presence true:
+  
 end
